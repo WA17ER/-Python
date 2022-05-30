cc5922d727309d1f8b2fbf0e401ad7b84679178d
Denis_Lebedev <darthvalter@yandex.com>
2022-05-30 15:23:41 +0300

    Объектно-ориентированное программирование

---
+++ pythonProject/3.py
@@ -0,0 +1,28 @@
+# Реализовать базовый класс Worker (работник), в котором определить атрибуты: name, surname, position (должность),
+# income (доход). Последний атрибут должен быть защищенным и ссылаться на словарь, содержащий элементы: оклад и
+# премия, например, {"wage": wage, "bonus": bonus}. Создать класс Position (должность) на базе класса Worker. В
+# классе Position реализовать методы получения полного имени сотрудника (get_full_name) и дохода с учетом премии (
+# get_total_income). Проверить работу примера на реальных данных (создать экземпляры класса Position,
+# передать данные, проверить значения атрибутов, вызвать методы экземпляров).
+
+class Worker():
+    def __init__(self, name, surname, position, income):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = income
+
+
+class Position(Worker):
+    def get_full_name(self):
+        print(f'Имя сотрудника {self.name}, Фаммилия {self.surname}')
+
+    def get_total_income(self):
+        print(f'Доход сотрудника равен {sum(list(self._income.values()))}')
+
+worker1_inc = {"wage": 15000, "bonus": 2000}
+
+worker1 = Position('Иванов','Иван','Менеджер', worker1_inc)
+worker1.get_full_name()
+worker1.get_total_income()
+
